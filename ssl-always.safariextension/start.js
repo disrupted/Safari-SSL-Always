d12af93bcaf034a49f034fa93829cf6811ef1782
disrupted <salomon.popp@gmail.com>
2017-01-28 19:24:48 +0100

    [fix] Js syntax

--- ssl-always.safariextension/start.js
+++ ssl-always.safariextension/start.js
@@ -4,7 +4,7 @@
  */
 if (document.location.protocol == "http:") {
   var httpLoad = document.createEvent("Event");
-  httpLoad.initEvent("load", true, true)
+  httpLoad.initEvent("load", true, true);
   var redirectTo = safari.self.tab.canLoad(httpLoad, document.location.href);
   if (redirectTo) {
     document.location = redirectTo;
